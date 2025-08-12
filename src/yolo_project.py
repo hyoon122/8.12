722e99f6b31ca7a2c61a3027f28ffa5486fe5306
hyoon122 <hoyoonjo6@gmail.com>
2025-08-12 12:04:07 +0900

    박스 그리기 추가

--- src/yolo_project.py
+++ src/yolo_project.py
@@ -27,4 +27,11 @@ while True:
     people = [det for det in results.boxes.data if int(det[5]) == 0]
     person_count = len(people)
 
+    # 박스 그리기
+    for det in people:
+        x1, y1, x2, y2, conf, cls = det
+        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+        cv2.putText(frame, f'Person {conf:.2f}', (int(x1), int(y1) - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
     
